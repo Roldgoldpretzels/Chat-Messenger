0234edb73510354663b35347c39e19008ecd543a
Roldgoldpretzels <100233541+Roldgoldpretzels@users.noreply.github.com>
2022-10-05 02:44:38 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer, Auth} from './components';
+
+import './App.css'
+
+const apiKey = 'sfetgx5vpfz8';
+
+const authToken = false;
+
+const client = StreamChat.getInstance(apiKey);
+const App = () => {
+    if(!authToken) return <Auth/>
+        return (
+            <div className = "app__wrapper">
+                <Chat client ={client} theme ="team light"> 
+                    <ChannelListContainer
+
+                    />
+                    <ChannelContainer
+
+                    />
+                </Chat>
+            </div>
+        );
+}
+export default App;
